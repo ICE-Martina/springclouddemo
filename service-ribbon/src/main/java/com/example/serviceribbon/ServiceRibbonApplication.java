e705a18b965982db1c9be3e620a9bfaf4ecf8aca
ash-ali <xulinjie0105@gmail.com>
2018-07-29 10:36:45 +0800

    linjie-实现了ribbon断路器Hystrix功能

--- service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/ServiceRibbonApplication.java
@@ -5,9 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * ribbon启动类
+ */
+//@EnableHystrix 开启断路器Hystrix
+@EnableHystrix
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
--- service-ribbon/src/main/java/com/example/serviceribbon/service/impl/RibbonServiceImpl.java
+++ service-ribbon/src/main/java/com/example/serviceribbon/service/impl/RibbonServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.serviceribbon.service.impl;
 
 import com.example.serviceribbon.service.IRibbonService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,11 +17,20 @@ public class RibbonServiceImpl implements IRibbonService {
 
     /**
      * 用程序名代替了具体的URL
+     * @HystrixCommand(fallbackMethod = "clientError")该注解对该方法创建了熔断器的功能，并fallbackMethod指定了熔断方法
      * @param name
      * @return
      */
+    @HystrixCommand(fallbackMethod = "clientError")
     @Override
     public String ribbonService(String name) {
         return restTemplate.getForObject("http://SERVICE-CLIENT/service-client?name="+name,String.class);
+    }
+
+    /**
+     * 熔断方法
+     */
+    public String clientError(String name){
+        return name + ",sorry error!";
     }
 }
